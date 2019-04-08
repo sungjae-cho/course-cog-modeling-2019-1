68bd8bd64cc6a8c0b3d9c746f69d1ae25503656c
Sungjae Cho <sungjae.cho.1118@gmail.com>
2019-04-08 22:35:02 +0900

    Add files of assignment 7.

---
+++ hw7.R
@@ -0,0 +1,22 @@
+# Homework 7-2
+# Plot two equations
+# Author: Sungjae Cho
+# Date: 2019-04-08
+
+
+model <- function(t) {
+  x = t - 1 + t^(-1) - t^(-2) / 2
+  return(x)
+}
+t = seq(-1, 1, by=0.001)
+x = model(t)
+plot(t, x, type="l")
+
+
+model <- function(t) {
+  x = t^2 - t / 2
+  return(x)
+}
+t = seq(-1, 1, by=0.001)
+x = model(t)
+plot(t, x, type="l")
